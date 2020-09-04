c9ac55d4cb3d02b425d519c509e428469e5d4ad1
Anusha Prasad <43382394+aprasad888@users.noreply.github.com>
2020-09-04 19:59:28 -0400

    Update AlarmObject.java

--- app/src/main/java/com/example/juniordesigntest/AlarmObject.java
+++ app/src/main/java/com/example/juniordesigntest/AlarmObject.java
@@ -2,21 +2,48 @@ package com.example.juniordesigntest;
 
 import android.os.CountDownTimer;
 
+import java.util.Calendar;
+import java.util.TimeZone;
+
 public class AlarmObject {
 
     private String alarmName;
-//    private long timeLeft;
     private long startTime;
     private long expirationTime;
     private long timerLength;
+    private String timerType;
+    private long hours;
+    private long minutes;
+    private long DAY_AS_MILLI = 24 * 60 * 60 * 1000;
 
-    AlarmObject(String alarmName, long timerLength) {
+
+    AlarmObject(String alarmName, long timerLength, String timerType, long hours, long minutes) {
         this.alarmName = alarmName;
         this.startTime = System.currentTimeMillis();
-        this.expirationTime = System.currentTimeMillis() + timerLength;
-        this.timerLength = timerLength;
-//        this.timeLeft = timeLeft;
-//        this.startTime = startTime;
+        if (timerType.equals("countdown")) {
+            this.expirationTime = System.currentTimeMillis() + timerLength;
+        } else {
+            Calendar cal = Calendar.getInstance();
+            cal.set(Calendar.HOUR_OF_DAY, (int) hours);
+            cal.set(Calendar.MINUTE, (int) minutes);
+            TimeZone tz = TimeZone.getTimeZone("GMT");
+            cal.setTimeZone(tz);
+            this.expirationTime = cal.getTimeInMillis();
+            this.hours = hours;
+            this.minutes = minutes;
+        }
+        if (timerType.equals("countdown")) {
+            this.timerLength = timerLength;
+        } else {
+            long remainingAlarmTime = 0;
+            if (getExpirationTime() < System.currentTimeMillis()) {
+                remainingAlarmTime = DAY_AS_MILLI - (System.currentTimeMillis() - getExpirationTime());
+            } else {
+                remainingAlarmTime = getExpirationTime() - System.currentTimeMillis();
+            }
+            this.timerLength = remainingAlarmTime;
+        }
+        this.timerType = timerType;
     }
 
     String getAlarmName() {
@@ -34,4 +61,10 @@ public class AlarmObject {
     long getTimerLength() {
         return timerLength;
     }
+
+    String getTimerType() { return timerType; }
+
+    String getAlarmTime() { return String.format("%02d:%02d", hours, minutes); }
+
+
 }
